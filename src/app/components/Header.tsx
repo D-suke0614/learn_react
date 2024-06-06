a5d8b274b51004397b7c767e77b2a2187e2449c8
Daisuke <daisuke.kida@nijibox.co.jp>
2024-06-06 17:30:13 +0900

    fix: ヘッダーテキストの幅を最適化

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Header = () => {
   return (
     <header className='bg-blue-200 py-8 px-6 shadow-lg shadow-blue-300'>
-      <h1 className='text-2xl font-bold hover:opacity-60'>
+      <h1 className='w-fit text-2xl font-bold hover:opacity-60'>
         <Link href={'/'}>LEARN REACT.JS</Link>
       </h1>
     </header>
