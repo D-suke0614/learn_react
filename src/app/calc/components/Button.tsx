3993038d07ce849f2dae309bc9007296b299812d
Daisuke <d.suke.kida@gmail.com>
2024-06-09 23:13:16 +0900

    fix: button style

--- src/app/calc/components/Button.tsx
+++ src/app/calc/components/Button.tsx
@@ -5,7 +5,11 @@ type PropsType = {
 }
 
 const Button = ({ text }: PropsType) => {
-  return <button className='py-5 px-7 rounded-[50%] bg-gray-400'>{text}</button>
+  return (
+    <button className='p-8 rounded-[50%] bg-gray-400 relative'>
+      <span className='absolute translate-y-[-50%] translate-x-[-50%]'>{text}</span>
+    </button>
+  )
 }
 
 export default Button
