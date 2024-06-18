351c042718187881a4a3e4e32f149ec7f3fb2fd5
Daisuke <daisuke.kida@nijibox.co.jp>
2024-06-18 14:58:03 +0900

    feat: 天気画像の追加

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -1,6 +1,6 @@
 export const GET = async () => {
   const url = 'https://weather.tsukumijima.net/api/forecast?city=130010' // tokyo
-  const res = await fetch(url)
+  const res = await fetch(url, {next: {revalidate: 3600}})
   const weather = await res.json()
   return Response.json({weather})
 }
--- src/app/weather/components/Card.tsx
+++ src/app/weather/components/Card.tsx
@@ -1,26 +1,27 @@
+import Image from 'next/image'
+import { Weather } from '../page'
+
 type PropsType = {
-  weather: {
-    dateLabel: string
-    weatherLabel: string
-    temperature: {
-      min: {
-        celsius: number
-      }
-      max: {
-        celsius: number
-      }
-    }
-  }
+  weather: Weather
 }
 
 const Card = ({ weather }: PropsType) => {
-  const { dateLabel, weatherLabel, temperature } = { ...weather }
+  const { date, dateLabel, weatherLabel, temperature, image } = { ...weather }
   const minTemp = temperature.min.celsius
   const maxTemp = temperature.max.celsius
   return (
     <div className='w-full shadow-md bg-gray-100 p-4 text-center'>
       <h3 className='font-bold text-lg'>{dateLabel}</h3>
-      <div>{/* <Image /> */}</div>
+      <div className='text-gray-500'>{date}</div>
+      <div className='my-4'>
+        <Image
+          className='my-0 mx-auto'
+          src={image.url}
+          width={image.width}
+          height={image.height}
+          alt='weather-image'
+        />
+      </div>
       <div>{weatherLabel}</div>
       <div className='text-gray-500'>{minTemp ? `${minTemp}℃ / ${maxTemp}℃` : `${maxTemp}℃`}</div>
     </div>
